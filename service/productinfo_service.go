24c60a7b4deee6da79cef6710d5949a08976f8ee
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-06-14 18:07:49 +0530

    feat: added productInfo service using protobuf and gRPC

---
+++ service/productinfo_service.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	pb "productInfo/service/ecommerce"
+
+	"github.com/gofrs/uuid/v5"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type server struct {
+	pb.UnimplementedProductInfoServer
+	productMap map[string]*pb.Product
+}
+
+// AddProduct implements ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,
+			"Error while generating Product ID : %v", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+// GetProduct implements ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+	return nil, status.Errorf(codes.NotFound, "Product does not exist with ID : %s.", in.Value)
+}
