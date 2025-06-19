540af5e04b4887cfe03145ddd250677062d4f31b
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-06-19 08:44:53 +0530

    feat: added server-side implementation for serachOrders method

--- service/productinfo_service.go
+++ service/productinfo_service.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	pb "productInfo/service/ecommerce"
+	"strings"
 
 	"github.com/gofrs/uuid/v5"
 	"google.golang.org/grpc/codes"
@@ -45,4 +48,22 @@ func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product,
 func (s *server) GetOrder(ctx context.Context, orderId *wrapperspb.StringValue) (*pb.Order, error){
 	ord := s.orderMap[orderId.Value]
 	return ord, nil
+}
+
+func (s *server) SearchOrders(searchQuery *wrapperspb.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
+	for key,order := range s.orderMap {
+		log.Print(key, order)
+		for _, itemStr := range order.Items {
+			log.Print(itemStr)
+			if strings.Contains(itemStr, searchQuery.Value) {
+				err := stream.Send(order)
+				if err != nil {
+				   return fmt.Errorf("error sending message to stream : %v",err) 
+				}
+				log.Print("Matching Order Found : " + key)
+				break
+			}
+		}
+	}
+	return nil
 }
